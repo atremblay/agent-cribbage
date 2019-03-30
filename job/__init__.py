039052f0e955cfbf6399d5ea5cf38aa5e9784ca1
gbmarc1 <marcantoinebelanger.gbmarc@gmail.com>
2019-03-30 19:38:14 -0400

    init fix

--- job/__init__.py
+++ job/__init__.py
@@ -7,10 +7,15 @@ pwd = dirname(__file__)
 
 sys.path.append(pwd)
 common = commonprefix([pwd, os.getcwd()])
-parent = '.'.join(pwd[len(common)+1:].split(os.sep)[1:])
+parent_module = '.'.join(pwd[len(common)+1:].split(os.sep)[1:])
+parent_script = '.'.join(pwd[len(common)+1:].split(os.sep))
 for x in glob(join(pwd, '*.py')):
     if not x.startswith('__'):
-        __import__(parent+'.' + basename(x)[:-3], globals(), locals())
+        # If called as a module
+        try:
+            __import__(parent_module+'.' + basename(x)[:-3], globals(), locals())
+        except ModuleNotFoundError:  # If called as a script
+            __import__(parent_script + '.' + basename(x)[:-3], globals(), locals())
 
 sys.path.remove(pwd)
 
