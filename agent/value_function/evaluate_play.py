6cc81767ba7c70611fe08102813f923fd0dcb4a4
gbmarc1 <marcantoinebelanger.gbmarc@gmail.com>
2019-03-30 23:36:35 -0400

    evaluate_play

--- agent/value_function/evaluate_play.py
+++ agent/value_function/evaluate_play.py
@@ -1,29 +1,25 @@
 from .value_function import ValueFunction
 from .register import register
-from gym_cribbage.envs.cribbage_env import evaluate_cards, Card, Stack
+from gym_cribbage.envs.cribbage_env import evaluate_table, Card, Stack
 import numpy as np
 import torch
 
 
 @register
-class EvalCards(ValueFunction):
+class EvalPlays(ValueFunction):
     def __init__(self, out_channels=15):
         """
         """
         super().__init__()
         self.custom_hash = __name__ + 'V0.0.0'
 
-    @staticmethod
-    def stack_to_numpy(stacks, state, env):
-        return [np.array([p.state for p in stacks])]
-
-    def forward(self, stacks):
+    def forward(self, stacks, discarded):
         values = torch.zeros(len(stacks), dtype=torch.float)
         for i, stack in enumerate(stacks.numpy()):
             myStack = Stack()
-            for card_idx in np.argwhere(stack == True):
+            for card_idx in np.argwhere(np.sum(stack, axis=0) == True):
                 myStack.add_(Card(*Card.rank_suit_from_idx(int(card_idx))))
 
-            values[i] = torch.tensor(evaluate_cards(myStack), dtype=torch.float)
+            values[i] = torch.tensor(evaluate_table(myStack), dtype=torch.float)
 
         return values
