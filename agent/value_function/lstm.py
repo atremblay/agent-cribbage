67fe3757973082d39d50bb478aff68cc895899c6
Alexis Tremblay <atremblay@explorance.com>
2019-03-31 13:24:29 -0400

    add simple lstm

--- agent/value_function/lstm.py
+++ agent/value_function/lstm.py
@@ -36,3 +36,37 @@ class LSTM(ValueFunction):
         out = out[:, -1, :]  # Only keeps last value of sequence
         out = self.clf(torch.cat((out, discarded), dim=1))
         return out
+
+
+@register
+class SimpleLSTM(ValueFunction):
+    def __init__(self):
+        """
+        Simple LSTM that only takes the cards face up. We do not include
+        face down here, thereby reducing the number of possible states.
+        """
+        super().__init__()
+
+        self.lstm = nn.SimpleLSTM(
+            input_size=52,
+            hidden_size=104,
+            num_layers=2,
+            batch_first=True
+        )
+
+        # Logistic Regression
+        self.clf = nn.Sequential(
+            nn.Linear(104, 52),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(52, 1),
+            nn.ReLU(True),
+        )
+        # Before applying weights
+        self.custom_hash = __name__ + 'V0.0.0'  # Change version when network is changed
+        self.apply(self.weights_init)
+
+    def forward(self, x):
+        out, (hidden, cell) = self.lstm(x)
+        out = out[:, -1, :]  # Only keeps last value of sequence
+        return out
