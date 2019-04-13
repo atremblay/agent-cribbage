aa3cc7eb443eac1f05552406e0a97863c37441d7
Alexis Tremblay <atremblay@explorance.com>
2019-04-13 12:36:12 -0400

    Add table to after state

--- agent/value_function/evaluate_play.py
+++ agent/value_function/evaluate_play.py
@@ -18,7 +18,8 @@ class EvalPlays(ValueFunction):
         values = torch.zeros(len(stacks), dtype=torch.float)
         for i, stack in enumerate(stacks.cpu().numpy()):
             myStack = Stack()
-            for card_idx in np.argwhere(np.sum(stack, axis=0) == True):
+            for card_idx in np.argmax(stack, axis=1):
+            # for card_idx in np.argwhere(np.sum(stack, axis=0) == True):
                 myStack.add_(Card(*Card.rank_suit_from_idx(int(card_idx))))
 
             values[i] = torch.tensor(evaluate_table(myStack), dtype=torch.float)
@@ -26,7 +27,17 @@ class EvalPlays(ValueFunction):
         return values
 
     def get_after_state(self, state, env):
-        choices = np.expand_dims(np.array([c.state for c in state.hand]), axis=1)
-        # Store state for data generation.
-        after_state = [choices.astype('float32')]
-        return after_state
+        after_state = np.zeros(
+            (len(state.hand), len(env.table) + 1, 52),
+            dtype=np.float32
+        )
+        for i, card in enumerate(state.hand):
+            table = Stack.from_stack(env.table)
+            table.add_(card)
+            for j, card_on_table in enumerate(table):
+                after_state[i, j] = card_on_table.state
+
+        # choices = np.expand_dims(np.array([c.state for c in state.hand]), axis=1)
+        # # Store state for data generation.
+        # after_state = [choices.astype('float32')]
+        return [after_state]
